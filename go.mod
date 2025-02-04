61a864571e10c486629c0dc20884e072f882a985
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-04 00:22:37 -0300

    http server added

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/alex-arraga/apple_store
 
 go 1.23
 
-require github.com/google/uuid v1.6.0
+require github.com/go-chi/chi/v5 v5.2.0
+
+require github.com/go-chi/cors v1.2.1
--- main.go
+++ main.go
@@ -2,13 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
-	"github.com/google/uuid"
-
-	"github.com/alex-arraga/apple_store/hello"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func main() {
-	fmt.Print(hello.Greet())
-	fmt.Print("New uuid:" + uuid.New().String())
+	fmt.Print("Starting server")
+
+	port := os.Getenv("PORT")
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hellow world"))
+		if err != nil {
+			log.Fatalf("Error in '/' route: %s", err)
+		}
+	})
+
+	s := &http.Server{
+		Handler:      router,
+		Addr:         ":" + port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Server failed: %s", err)
+	}
 }
